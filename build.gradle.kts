0bda7880fc99b793ef1b679305f3a560dda43e43
thopkins568-glitch <thopkins568@gmail.com>
2025-12-09 21:48:25 -0600

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    kotlin("multiplatform") version "1.9.10" apply false
+    id("com.android.application") version "8.1.0" apply false
+    id("com.android.library") version "8.1.0" apply false
+}
